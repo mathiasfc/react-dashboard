3c49fd4eeb0c853d38151571996a477f4a27e13f
mathiasfc <mathias.falci@gmail.com>
2018-12-21 11:28:35 -0200

    Chart 80%

--- app/components/Traffic/traffic.jsx
+++ app/components/Traffic/traffic.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-const { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } = Recharts;
+const { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } = Recharts;
 import styles from './traffic.less';
 import axios from 'axios';
 
@@ -31,25 +31,33 @@ class Traffic extends Component {
     this.fetchData();
   }
 
-  createStructure() {
-    debugger;
+  createStructure() {}
+
+  loading() {
+    if (this.state.months.length > 0) {
+      return (
+        <ResponsiveContainer>
+          <AreaChart data={this.state.months} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Area type="monotone" dataKey="views" stroke="#8884d8" fill="#8884d8" />
+          </AreaChart>
+        </ResponsiveContainer>
+      );
+    }
+    return (
+      <div className="loading">
+        <span>
+          <i className="fas fa-spinner fa-spin" />
+        </span>
+      </div>
+    );
   }
 
   render() {
-    const data = [{ name: 'Page A', uv: 4000, pv: 2400, amt: 2400 }, { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 }, { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 }, { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 }, { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 }, { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 }, { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 }];
-    return (
-      <div className={styles.container}>
-        <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-          <XAxis dataKey="name" />
-          <YAxis />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-        </LineChart>
-      </div>
-    );
+    return <div className={styles.container}>{this.loading()}</div>;
   }
 }
 
