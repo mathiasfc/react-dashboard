e7bacf7afca1b3e6c048110b0f8f26c4b10c809f
mathiasfc <mathias.falci@gmail.com>
2018-12-21 15:26:08 -0200

    Chat 40%

--- app/components/Chat/chat.jsx
+++ app/components/Chat/chat.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styles from './chat.less';
 import axios from 'axios';
+import Message from '../Message/message';
 
 class Chat extends Component {
   constructor(props) {
@@ -30,11 +31,22 @@ class Chat extends Component {
     this.loadMessages();
   }
 
-  createStructure() {}
+  createStructure() {
+    let messages = this.state.messages;
+    let structure = [];
+
+    messages.forEach(function(msg) {
+      //console.log(msg);
+      //let className = msg.displayPortraitLeft ? 'imgLeft' : 'imgRight';
+      structure.push(<Message msg={msg} />);
+    });
+
+    return structure;
+  }
 
   loading() {
     if (this.state.messages.length > 0) {
-      return <div />;
+      return <div className="chatContainer">{this.createStructure()}</div>;
     }
     return (
       <div className="loading">
@@ -48,10 +60,12 @@ class Chat extends Component {
   render() {
     return (
       <div className={styles.container}>
-      <div className="header">
-        <i className="far fa-comments"></i><span className={styles.chatTitle}>Chat</span>
+        <div className="header">
+          <i className="far fa-comments" />
+          <span>Chat</span>
         </div>
         <hr />
+        {this.loading()}
       </div>
     );
   }
---
+++ app/components/Message/message.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import styles from './message.less';
+
+class Message extends Component {
+  createMsgBlock() {
+    const userName = this.props.msg.userName;
+    const portrait = this.props.msg.portrait;
+    const message = this.props.msg.message;
+    const orientation = this.props.msg.displayPortraitLeft ? 'imgLeft' : 'imgRight';
+    const time = this.props.msg.time;
+
+    if (userName) {
+      return (
+        <div className={styles.msgBlock}>
+          <div className={orientation}>
+            <img className="userImg" src={portrait} />
+            <div className="msgTitle">
+              <span className="userName">{userName}</span>
+              <span className="msgTime">{time}</span>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  formatName(name) {
+    const dictNames = {
+      newOrders: 'New Orders',
+      comments: 'Comments',
+      newUsers: 'New Users',
+      pageViews: 'Page Views'
+    };
+
+    return dictNames[name];
+  }
+
+  render() {
+    return <div>{this.createMsgBlock()}</div>;
+  }
+}
+
+export default Message;
